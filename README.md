8c112e049f55196812efa693d7d8dafbde6e0a5f
Charles Moulliard <cmoulliard@redhat.com>
2018-03-08 11:15:56 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,47 @@
+# Install OpenShift Cluster using ansible & CentOS VM provisioned on Virtualbox
+
+- Create Virtualbox vm using the `up.sh` bash script
+```bash
+./up.sh
+```
+
+- Secure copy your public key
+```bash
+sshpass -f pwd.txt ssh root@127.0.0.1 -p 5222 "mkdir ~/.ssh && chmod 700 ~/.ssh && touch ~/.ssh/authorized_keys && chmod 600 ~/.ssh/authorized_keys"
+sshpass -f pwd.txt ssh-copy-id -n -i ~/.ssh/id_rsa.pub root@127.0.0.1 -p 5222
+```
+
+- Install Python3
+```bash
+ssh root@127.0.0.1 -p 5222 "yum -y install https://centos7.iuscommunity.org/ius-release.rpm && yum -y install python36u"
+```
+
+- Git clone `openshihift-ansible` 
+```bash
+git clone -b release-3.7 https://github.com/openshift/openshift-ansible.git
+```
+
+- Import RPMs of OpenShift
+```bash
+ansible-playbook -i inventory playbook/install-package.yaml -e openshift_node=masters
+```
+
+- Enable NetworkManager
+```bash
+ansible-playbook -i inventory playbook/enable_network-manager.yml -e openshift_node=masters
+```
+
+- Create OpenShift cluster
+```bash
+ansible-playbook -i inventory openshift-ansible/playbooks/byo/config.yml
+```
+
+- Enable NetworkManager
+```bash
+ansible-playbook -i inventory playbook/enable_cluster_admin.yml -e openshift_node=masters
+```
+
+- Setup persistence
+```bash
+ansible-playbook -i inventory playbook/setup_persistence.yml -e openshift_node=masters
+```
---
+++ up.sh
@@ -0,0 +1,90 @@
+#!/bin/bash
+# written by Charles Moulliard
+# 8-March-2018
+#
+# Variables below are passed to VBoxManage
+# Modify appropriately
+#
+# You can ssh to the VM : ssh root@127.0.0.1 -p 5222
+# user : root
+# pwd : centos
+#
+ISO_NAME="centos7.iso"
+ISO="$(pwd)/$ISO_NAME" # MiniShift ISO Image - https://github.com/minishift/minishift-centos-iso/releases/download/v1.7.0/minishift-centos7.iso
+virtualbox_vm_name="CentOS-7.1" # VM Name
+OSTYPE="RedHat";
+DISKSIZE=20480; #in MB
+RAM=2048; #in MB
+CPU=2;
+CPUCAP=100;
+PAE="off";
+VRAM=8;
+USB="off";
+
+echo "Getting ISO image if not there"
+if [ -f $ISO_NAME ]; then
+   echo "######### ISO File $FILE exists."
+else
+   echo "######### Getting file"
+   wget -O $ISO_NAME https://github.com/minishift/minishift-centos-iso/releases/download/v1.7.0/minishift-centos7.iso
+fi
+
+echo "######### Poweroff machine if it runs"
+vboxmanage controlvm $virtualbox_vm_name poweroff
+echo "######### .............. Done"
+
+echo "######### unregister vm "$virtualbox_vm_name" and delete it"
+vboxmanage unregistervm $virtualbox_vm_name --delete || echo "No VM by name ${virtualbox_vm_name}"
+
+####################################################################
+echo "######### Create vboxnet0 network and set dhcp server : 192.168.99.0/24"
+vboxmanage hostonlyif remove vboxnet0
+vboxmanage hostonlyif create
+vboxmanage hostonlyif ipconfig vboxnet0 --ip 192.168.99.1 --netmask 255.255.255.0
+vboxmanage dhcpserver add --ifname vboxnet0 --ip 192.168.99.20 --netmask 255.255.255.0 --lowerip 192.168.99.50 --upperip 192.168.99.200
+vboxmanage dhcpserver modify --ifname vboxnet0 --enable
+
+##########################################
+echo "######### Create VM"
+vboxmanage createvm --name ${virtualbox_vm_name} --ostype "RedHat_64" --register --basefolder=$HOME/VirtualBox\ VMs
+
+# VirtualBox Network
+vboxmanage modifyvm ${virtualbox_vm_name} \
+        --nic1 hostonly --hostonlyadapter1 vboxnet0 \
+        --nic2 nat
+
+# VM Config
+vboxmanage modifyvm ${virtualbox_vm_name} --memory "$RAM";
+vboxmanage modifyvm ${virtualbox_vm_name} --boot1 dvd --boot2 disk --boot3 none --boot4 none;
+vboxmanage modifyvm ${virtualbox_vm_name} --chipset piix3;
+vboxmanage modifyvm ${virtualbox_vm_name} --ioapic on;
+vboxmanage modifyvm ${virtualbox_vm_name} --mouse ps2;
+vboxmanage modifyvm ${virtualbox_vm_name} --cpus "$CPU" --cpuexecutioncap "$CPUCAP" --pae "$PAE";
+vboxmanage modifyvm ${virtualbox_vm_name} --hwvirtex off --nestedpaging off;
+
+vboxmanage modifyvm ${virtualbox_vm_name} --vram "$VRAM";
+vboxmanage modifyvm ${virtualbox_vm_name} --monitorcount 1;
+vboxmanage modifyvm ${virtualbox_vm_name} --accelerate2dvideo off --accelerate3d off;
+vboxmanage modifyvm ${virtualbox_vm_name} --audio none;
+
+vboxmanage modifyvm ${virtualbox_vm_name} --clipboard bidirectional;
+vboxmanage modifyvm ${virtualbox_vm_name} --usb "$USB";
+vboxmanage modifyvm ${virtualbox_vm_name} --vrde on;
+
+# VirtualBox - Attach ISO image to IDE
+vboxmanage storagectl ${virtualbox_vm_name} --name "IDE" --add ide --controller PIIX4 --bootable on
+vboxmanage storageattach ${virtualbox_vm_name} --storagectl IDE --port 0 --device 0 --type dvddrive --medium $ISO
+
+# VirtualBox HDD
+vboxmanage storagectl ${virtualbox_vm_name} --name "SATA" --add sata --controller IntelAhci --bootable on
+
+# VirtualBox HDD - Config used to boot the first time the ISO !
+#vboxmanage createmedium --filename "$HOME/VirtualBox\ VMs/${virtualbox_vm_name}.vdi" --size "$DISKSIZE"
+#vboxmanage storagectl ${virtualbox_vm_name} --name "SATA" --add sata --controller IntelAhci --bootable on
+#vboxmanage storageattach ${virtualbox_vm_name} --storagectl "SATA" --type hdd --port 0 --device 0 --medium "$HOME/VirtualBox\ VMs/${virtualbox_vm_name}.vdi"
+
+vboxmanage startvm ${virtualbox_vm_name} --type headless
+vboxmanage controlvm ${virtualbox_vm_name} natpf2 ssh,tcp,127.0.0.1,5222,,22
+
+echo "######### Run 'vboxmanage list -l vms | less' to check configuration."
+exit 0;
