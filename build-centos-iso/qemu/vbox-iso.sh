898db6f763ec0e7527950de19878a95032d2f7df
Charles Moulliard <cmoulliard@redhat.com>
2018-03-14 18:42:32 +0100

    Use a template to get your local public key and add it to the user-data

--- build-centos-iso/qemu/vbox-iso.sh
+++ build-centos-iso/qemu/vbox-iso.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
-IMAGE_DIR="/Users/dabou/images"
+LOCAL_HOME_DIR=$1
+IMAGE_DIR=${LOCAL_HOME_DIR}/images
 CENTOS_QCOW2="CentOS-7-x86_64-GenericCloud-1802.qcow2c"
 OS_NAME="centos7"
+
+##
+## Add public key
+##
+add_ssh_key(){
+    echo "##### Add public key and create user-data file"
+    YOUR_SSH_KEY=$(cat ${LOCAL_HOME_DIR}/.ssh/id_rsa.pub)
+    sed "s|SSH_PUBLIC_KEY|${YOUR_SSH_KEY}|g" user-data.tpl > user-data
+}
 
 ##
 ## generate the config-drive iso
@@ -28,6 +38,7 @@ make_vmdk(){
     qemu-img convert -p -f qcow2 ${IMAGE_DIR}/${CENTOS_QCOW2} -O vmdk ${IMAGE_DIR}/${OS_NAME}.vmdk
 }
 
+add_ssh_key
 gen_iso
 #resize
 make_vmdk
