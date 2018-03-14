38c49efa1ff771c6385b19aa5a283c6e9dd4d406
Charles Moulliard <cmoulliard@redhat.com>
2018-03-14 11:47:31 +0100

    ISO file created but when vm is created and booted, then we get a besh permission denied

--- build-centos-iso/new-iso-2.sh
+++ build-centos-iso/new-iso-2.sh
@@ -1,35 +1,28 @@
 #!/usr/bin/env bash
 
 LOCAL_HOST="/Users/dabou/images"
-CENTOS_ISO="CentOS-7-x86_64-Minimal-1708.iso"
 ISO_NAME="centos7"
-ISO="$LOCAL_HOST/$CENTOS_ISO"
 BUILD_DIR=/tmp/build
+CACHE_DIR=/tmp/cache
 HOME_INSTALL=$(pwd)
 
 echo "##### Create our own ISO ....."
 echo "##### using $HOME_INSTALL/new-iso-2.sh script"
 
-echo "##### Remove bootiso folder"
-rm -rf /tmp/{bootiso,build}
+echo "##### Remove build folder"
+rm -rf /tmp/build
 
-echo "##### Make bootiso, build dirs"
-mkdir /tmp/{bootiso,build}
+echo "##### Make build directory"
+mkdir /tmp/build
 
-echo "##### Create iso using live-cdcreator"
+echo "##### Create ISO image using live-cdcreator and our customized kickstart config file"
 cd ${BUILD_DIR}
 
 sudo livecd-creator -v --tmpdir=${BUILD_DIR}/temp \
-                       --cache="/tmp/cache" \
-                       --config $HOME_INSTALL/config/centos-7-2.ks \
+                       --cache=${CACHE_DIR} \
+                       --config $HOME_INSTALL/config/centos-generated.ks \
                        --logfile=${BUILD_DIR}/livecd-creator.log \
-                       --fslabel centos7
-dd if=/dev/zero bs=2k count=1 of=${BUILD_DIR}/boot.iso
-dd if=${BUILD_DIR}/${ISO_NAME}.iso bs=2k skip=1 >> ${BUILD_DIR}/tmp.iso
-mv -f ${BUILD_DIR}/tmp.iso ${BUILD_DIR}/${ISO_NAME}.iso
+                       --fslabel ${ISO_NAME}
 
-echo "##### Add md5 signature"
-implantisomd5 ${BUILD_DIR}/${ISO_NAME}.iso
-
-echo "##### Copy iso file to your local_host : $LOCAL_HOST/$CENTOS_ISO"
+echo "##### Copy iso file to your local_host : $LOCAL_HOST/${ISO_NAME}.iso"
 cp ${BUILD_DIR}/${ISO_NAME}.iso $LOCAL_HOST/${ISO_NAME}.iso
